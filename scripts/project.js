2833da2dbf7ba55152a3af4af6f0f66b18b6e8fa
‘LuayYounus’ <luayghazwan@yahoo.com>
2017-03-04 23:51:43 -0800

    re-organizing and debugging

--- scripts/project.js
+++ scripts/project.js
@@ -49,6 +49,29 @@ Project.fetchAll = function(){
   }
 };
 
+'use strict';
+
+//jQuery to show the clicked tab
+
+$('.main-nav').on('click','.tab',function(){
+  $('main section').hide();
+  if ($(this).data('content') === 'home') {
+    $('main section').show();
+  } else {
+    $('#' + $(this).data('content')).fadeIn();
+  }
+});
+
+
+//append to Html
+function renderInstance(rawData){
+  console.log(rawData, 'rawdata');
+  rawData.map(function(a) {
+    console.log(a, 'a');
+    $('#projects').append(a.toHtml());
+  })
+}
+
 
 // Show/Hide hamburger list when clicked
 //
